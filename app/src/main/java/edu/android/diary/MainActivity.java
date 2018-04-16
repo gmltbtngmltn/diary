9aec0c697145a41d45bdcf893af2515665a70874
gmltbtngmltn <tndlfemd12@naver.com>
2018-04-16 11:16:00 +0900

    textView id 변경

--- app/src/main/java/edu/android/diary/MainActivity.java
+++ app/src/main/java/edu/android/diary/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView = findViewById(R.id.sds);
+        TextView textView = findViewById(R.id.textview);
     }
 }
