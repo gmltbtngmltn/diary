8913d4d9815a9f4aafd08595ea12f0cb8faab405
kangNatsu <djflsdhkdk@naver.com>
2018-04-16 13:56:16 +0900

    앱 시작 화면 + 애니메이션 나오는 부분 (이동 버튼)+ 일기 쓰는 부분 +일기쓰는 페이지 플로팅 버틍

--- app/src/main/java/edu/android/diary/MainActivity.java
+++ app/src/main/java/edu/android/diary/MainActivity.java
@@ -1,7 +1,9 @@
 package edu.android.diary;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView textView = findViewById(R.id.textview);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, WriteDiary.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/edu/android/diary/WriteDiary.java
+++ app/src/main/java/edu/android/diary/WriteDiary.java
@@ -6,24 +6,31 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.FrameLayout;
 
 public class WriteDiary extends AppCompatActivity {
+
+    FloatingActionButton fab, fab1, fab2, fab3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_diary);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
             }
         });
+
+
     }
 
 }
