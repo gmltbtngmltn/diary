b84be7cfafe104299f87bee1b0fb7dba3db7692b
seungminShinMark <neu-roses@hotmail.com>
2018-04-17 11:38:53 +0900

    make setting

--- app/src/main/java/edu/android/diary/DiarySetting.java
+++ app/src/main/java/edu/android/diary/DiarySetting.java
@@ -23,7 +23,7 @@ public class DiarySetting extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary_setting);
-
+        spinner = findViewById(R.id.spinner);
 
 
     }
