0650c6573c1ed349560cd94c2e08298be9a7e957
HolyRagingPower <soulmate106@naver.com>
2018-04-20 16:25:35 +0900

    diary 모델클래스 변경, diaryDao 기능 읽기 삭제 쓰기 구현 wryyyyyaaa!!!

--- app/src/main/java/edu/android/diary/Diary.java
+++ app/src/main/java/edu/android/diary/Diary.java
@@ -3,23 +3,26 @@ package edu.android.diary;
 public class Diary {
     private int photoId;//갤러리에서 받아온 사진파일을 바이너리 형식이든 파일명이든 어떤형식으로든 사진파일 정보를 담는 맴버변수
                           //지금은 임시로 int로 해놓음
-    private String txt; //일기장의 text내용을 담는 맴버변수
 
-    public Diary(int photoId, String txt) {
-        this.photoId = photoId;
+    private String photoPath;//사진파일경로를 담는 맴버변수
+    private String txt;//일기장의 text내용을 담는 맴버변수
+
+    public Diary(String photoPath, String txt) {
+        this.photoPath = photoPath;
         this.txt = txt;
     }
 
-    public void setPhotoId(int photoId) {
-        this.photoId = photoId;
+
+    public void setPhotoPath(String photoPath) {
+        this.photoPath = photoPath;
     }
 
     public void setTxt(String txt) {
         this.txt = txt;
     }
 
-    public int getPhotoId() {
-        return photoId;
+    public String getPhotoPath() {
+        return photoPath;
     }
 
     public String getTxt() {
--- app/src/main/java/edu/android/diary/DiaryDao.java
+++ app/src/main/java/edu/android/diary/DiaryDao.java
@@ -1,21 +1,35 @@
 package edu.android.diary;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 //각 diary 객체를 생성,삭제,수정,열람
 
 public class DiaryDao {
-    private static final int[] IMAGE_IDS = {
-            R.drawable.sample1, R.drawable.sample2, R.drawable.sample3,
-            R.drawable.sample4, R.drawable.sample5, R.drawable.sample6,
-            R.drawable.sample7, R.drawable.sample8, R.drawable.sample9,
-            R.drawable.sample10, R.drawable.sample11, R.drawable.sample12,
-            R.drawable.sample13
-    };//이부분은 향후에 갤러리에서 사진파일을 받아오는 부분으로 대체될 것임
+    private static final String TAG = "tttttt";
+
+    private static final String parentPath= Environment.getExternalStorageDirectory().getPath();
+    private static final String childPath=parentPath+"/custDir";
 
     private List<Diary> diaries=new ArrayList<>();
     private static DiaryDao instance=null;
+
+    File file=new File(childPath,"diary.dat");
 
     public static DiaryDao getInstance(){
         if(instance==null){
@@ -24,37 +38,147 @@ public class DiaryDao {
         return instance;
     }
 
-    private DiaryDao() {
-        makedata();
+    private DiaryDao(){
+        File filedir=new File(childPath);
+        if(!filedir.exists()){
+            filedir.mkdirs();
+        }else{
+            Log.i(TAG,"이미 폴더가 있음");
+            diaries = getContactList();
+        }
     }
 
-    ////////////
-    private void makedata() {
-        for (int i = 0; i < 100; i++) {
-            Diary contact = new Diary(IMAGE_IDS[i % IMAGE_IDS.length],"shit"+i);
-            diaries.add(contact);
-        }
-    }//임시
-    /////////////////
-
     public List<Diary> getContactList() {
+        InputStream in = null;
+        BufferedInputStream bin = null;
+        ObjectInputStream ois = null;
+        try {
+            in = new FileInputStream(file);
+            bin = new BufferedInputStream(in);
+            ois = new ObjectInputStream(bin);
+
+            diaries = (ArrayList<Diary>) ois.readObject();
+
+        } catch (Exception e) {
+            Log.e(TAG,e.getMessage());
+        } finally {
+            try {
+                ois.close();
+            } catch (Exception e) {
+                Log.e(TAG,e.getMessage());
+            }
+        }
         return diaries;
-    }//diary객체들을 전체 열람하는 부분
-    //diary의 Array에서 선택된 index의 정보를 볼때 =
-    // getInstance().getContactList().get(index).getPhotoId; , getInstance().getContactList().get(index).getTxt;
-    
-    public void writeDiary(int photoId, String diaryTxt) {
-        Diary contact = new Diary(photoId,diaryTxt);
-        diaries.add(contact);
-    }//diary객체들을 생성하는 부분
+    }//diary객체들을 읽는부분(1)
+
+    public Bitmap LoadImage(String imagename){
+        File fileB=new File(childPath,imagename);
+
+        InputStream InB = null;
+        BufferedInputStream binB = null;
+        try {
+            InB = new FileInputStream(fileB);
+            binB = new BufferedInputStream(InB);
+
+            return BitmapFactory.decodeStream(binB);
+        } catch (Exception e) {
+            Log.e(TAG,e.getMessage());
+        } finally {
+            try {
+                binB.close();
+            } catch (IOException e) {
+                Log.e(TAG,e.getMessage());
+            }
+        }
+        return null;
+    }//diary객체들을 읽는부분(2) (비트맵 이미지를 파일경로에서 읽어온다)
+
+
+    public void writeDiary(Bitmap bitmap, String imagename,String diaryTxt) {
+        File fileB=new File(childPath,imagename);
+
+        Diary diary = new Diary(imagename,diaryTxt);
+        diaries.add(diary);
+
+        OutputStream out = null;
+        BufferedOutputStream bout = null;
+        ObjectOutputStream oos = null;
+
+        OutputStream outB = null;
+        BufferedOutputStream boutB = null;
+
+        try {
+            out = new FileOutputStream(file,false);
+            bout = new BufferedOutputStream(out);
+            oos = new ObjectOutputStream(bout);
+
+            oos.writeObject(diaries);
+
+            outB = new FileOutputStream(fileB,false);
+            boutB = new BufferedOutputStream(outB);
+
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, boutB);
+        } catch (Exception e) {
+            Log.e(TAG,e.getMessage());
+        } finally {
+            try {
+                oos.close();
+                boutB.close();
+            } catch (IOException e) {
+                Log.e(TAG,e.getMessage());
+            }
+        }
+    }//diary객체들을 쓰는부분
 
     public void deleteDiary(int position){
+        //배열에서 삭제
         diaries.remove(position);
+
+        //변경된 배열을 파일에 다시 저장
+        OutputStream out = null;
+        BufferedOutputStream bout = null;
+        ObjectOutputStream oos = null;
+        try {
+            out = new FileOutputStream(file,false);
+            bout = new BufferedOutputStream(out);
+            oos = new ObjectOutputStream(bout);
+
+            oos.writeObject(diaries);
+        } catch (Exception e) {
+            Log.e(TAG,e.getMessage());
+        } finally {
+            try {
+                oos.close();
+            }catch (IOException e) {
+                Log.e(TAG,e.getMessage());
+            }
+        }
+
     }//diary객체들을 삭제하는 부분
 
-    public void updateDiary(int position,int photoId, String diaryTxt){
-        Diary diary=new Diary(photoId,diaryTxt);
+    public void updateDiary(int position,String imagename, String diaryTxt){
+        Diary diary=new Diary(imagename,diaryTxt);
         diaries.set(position,diary);
+
+        //변경된 배열을 파일에 다시 저장
+        OutputStream out = null;
+        BufferedOutputStream bout = null;
+        ObjectOutputStream oos = null;
+        try {
+            out = new FileOutputStream(file,false);
+            bout = new BufferedOutputStream(out);
+            oos = new ObjectOutputStream(bout);
+
+            oos.writeObject(diaries);
+        } catch (Exception e) {
+            Log.e(TAG,e.getMessage());
+        } finally {
+            try {
+                oos.close();
+            }catch (IOException e) {
+                Log.e(TAG,e.getMessage());
+            }
+        }
     }//diary객체들을 수정하는 부분
 
 }
