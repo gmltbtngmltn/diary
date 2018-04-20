779686c874a3a2045f7ac6ce2cfbbdea07d9ba66
HolyRagingPower <soulmate106@naver.com>
2018-04-20 17:08:46 +0900

    외부저장소 읽고 쓰는 권한 추가 wryyyyyaaa!!!

--- app/src/main/java/edu/android/diary/DiaryListActivity.java
+++ app/src/main/java/edu/android/diary/DiaryListActivity.java
@@ -1,10 +1,15 @@
 package edu.android.diary;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,13 +23,15 @@ public class DiaryListActivity extends AppCompatActivity {
 
     private RecyclerView recycler;
     private List<Diary> dataset;
-
+    private DiaryAdaptor adaptor;
+    private static final String TAG = "tttttt";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary_list);
 
         dataset = DiaryDao.getInstance().getContactList();
+        Log.i(TAG,""+dataset.size());
 
         recycler = findViewById(R.id.recycle);
 
@@ -32,8 +39,12 @@ public class DiaryListActivity extends AppCompatActivity {
 
         recycler.setLayoutManager(new LinearLayoutManager(this));
 
-        DiaryAdaptor adaptor = new DiaryAdaptor();
+        adaptor = new DiaryAdaptor();
 
+        recycler.setAdapter(adaptor);
+    }
+
+    private void gangshin(){
         recycler.setAdapter(adaptor);
     }
 
@@ -51,17 +62,61 @@ public class DiaryListActivity extends AppCompatActivity {
         }
 
         @Override
-        public void onBindViewHolder(@NonNull Diaryholder holder, int position) {
+        public void onBindViewHolder(@NonNull Diaryholder holder, final int position) {
             final Diary diary = dataset.get(position);
 
-            holder.imageView.setImageResource(diary.getPhotoId());
+            Bitmap bitmap=DiaryDao.getInstance().LoadImage(diary.getPhotoPath());
+            holder.imageView.setImageBitmap(bitmap);
             holder.textView.setText(diary.getTxt());
+
             holder.itemView.setOnClickListener(new View.OnClickListener() {
+
                 @Override
                 public void onClick(View v) {
-//                    Intent intent=DetailActivity.newIntent(MainActivity.this,position);
-//                    startActivity(intent);
-                    Toast.makeText(DiaryListActivity.this, "다음화면으로~", Toast.LENGTH_SHORT).show();
+                    Intent intent = DirayDetailActivity.newIntent(DiaryListActivity.this, position);
+                    startActivity(intent);
+                }
+
+            });
+
+            holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                            DiaryListActivity.this);
+
+                    // 제목셋팅
+                    alertDialogBuilder.setTitle("데이터 삭제");
+
+                    // AlertDialog 셋팅
+                    alertDialogBuilder
+                            .setMessage("하지 않겠는가?")
+                            .setCancelable(false)
+                            .setPositiveButton("삭제",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(
+                                                DialogInterface dialog, int id) {
+
+                                            DiaryDao.getInstance().deleteDiary(position);
+
+                                            Toast.makeText(DiaryListActivity.this, "삭제됨", Toast.LENGTH_SHORT).show();
+                                            dataset = DiaryDao.getInstance().getContactList();
+                                            gangshin();
+                                        }
+                                    })
+                            .setNegativeButton("취소",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(
+                                                DialogInterface dialog, int id) {
+                                            // 다이얼로그를 취소한다
+                                            dialog.cancel();
+                                        }
+                                    });
+                    // 다이얼로그 생성
+                    AlertDialog alertDialog = alertDialogBuilder.create();
+                    // 다이얼로그 보여주기
+                    alertDialog.show();
+                    return true;
                 }
             });
         }
