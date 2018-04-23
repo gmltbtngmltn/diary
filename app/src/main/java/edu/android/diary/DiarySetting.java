85e3ab65531c5456b970c724ac749560854094fc
seungminShinMark <neu-roses@hotmail.com>
2018-04-23 10:43:57 +0900

    수정

--- app/src/main/java/edu/android/diary/DiarySetting.java
+++ app/src/main/java/edu/android/diary/DiarySetting.java
@@ -1,7 +1,6 @@
 package edu.android.diary;
 
-import android.app.Application;
-import android.content.Context;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -18,7 +17,7 @@ import com.tsengvn.typekit.Typekit;
 public class DiarySetting extends AppCompatActivity {
 
 
-    private TextView textView1 , textView2 , textView3 ,textView4 , settingTitle;
+    private TextView textView1 , textView2 , textView3 ,textView4 , settingTitle , testText;
     private RadioGroup RadioGroup1,RadioGroup2,RadioGroup3;
     private RadioButton btn0,btn1,btn2,btn3,btn4,btn5,btn6;
     private Spinner spinner;
@@ -34,16 +33,20 @@ public class DiarySetting extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary_setting);
 
+
         //textView
         settingTitle = findViewById(R.id.settingTitle);
         textView1 = findViewById(R.id.textView1);
         textView2 = findViewById(R.id.textView2);
         textView3 = findViewById(R.id.textView3);
         textView4 = findViewById(R.id.textView4);
+        testText = findViewById(R.id.testText);
+
         //RadioGroup
         RadioGroup1 = findViewById(R.id.radioGroup1);
         RadioGroup2 = findViewById(R.id.radioGroup2);
         RadioGroup3 = findViewById(R.id.radioGroup3);
+
         //RadioButton
         btn0 = findViewById(R.id.btn0);
         btn1 = findViewById(R.id.btn1);
@@ -52,8 +55,10 @@ public class DiarySetting extends AppCompatActivity {
         btn4 = findViewById(R.id.btn4);
         btn5 = findViewById(R.id.btn5);
         btn6 = findViewById(R.id.btn6);
+
         //Spinner
         spinner = findViewById(R.id.spinner);
+
         //Button
         saveBtn = findViewById(R.id.saveBtn);
         cancelBtn = findViewById(R.id.cancelBtn);
@@ -73,7 +78,8 @@ public class DiarySetting extends AppCompatActivity {
          int id = RadioGroup1.getCheckedRadioButtonId();
          RadioButton cRb = findViewById(id);
          if(id == 1){
-
+            Typeface tf = Typeface.createFromAsset(getAssets(),"SangSangTitle.ttf");
+            testText.setTypeface(tf);
 
          }
 
