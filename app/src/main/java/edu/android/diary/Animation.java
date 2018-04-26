a76f45e70933a89444971d5b1c0a57080a417db2
nsa25638 <nsa25638@naver.com>
2018-04-26 18:41:34 +0900

    zzzzzzzzzzzzzzzzzzzz

--- app/src/main/java/edu/android/diary/Animation.java
+++ app/src/main/java/edu/android/diary/Animation.java
@@ -1,5 +1,16 @@
 package edu.android.diary;
 
+import android.nfc.Tag;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
@@ -7,20 +18,33 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.hitomi.cmlibrary.CircleMenu;
 import com.hitomi.cmlibrary.OnMenuSelectedListener;
 import com.hitomi.cmlibrary.OnMenuStatusChangeListener;
 
-public class Animation extends AppCompatActivity {
 
+import java.util.Calendar;
+
+public class Animation extends AppCompatActivity {
+    private int currentIndex;
     private CircleMenu circleMenu;
+private static int[] IMAGE_IDS={R.drawable.moon00,
+        R.drawable.moon01, R.drawable.moon02,
+        R.drawable.moon03, R.drawable.moon04,
+        R.drawable.moon05, R.drawable.moon06, R.drawable.moon07 };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation);
+
+        java.util.Calendar cal = java.util.Calendar.getInstance();
+        int year = cal.get(java.util.Calendar.YEAR);
+        int month = cal.get(java.util.Calendar.MONTH) + 1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
 
 
         circleMenu = (CircleMenu) findViewById(R.id.circle_menu);
@@ -73,4 +97,21 @@ public class Animation extends AppCompatActivity {
 
     }
 
+    public void btnPrev(View view) {
+        if (currentIndex > 0) {
+            currentIndex--;
+            view.setBackgroundResource(IMAGE_IDS[currentIndex]);
+        } else {
+            Toast.makeText(Animation.this, "첫번째 이미지", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void btnNext(View view) {
+        if (currentIndex < IMAGE_IDS.length - 1) {
+            currentIndex++;
+            view.setBackgroundResource(IMAGE_IDS[currentIndex]);
+        } else {
+            Toast.makeText(Animation.this, "마지막 이미지", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
