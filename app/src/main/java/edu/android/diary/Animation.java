a196a11cf37cbb55a9754f1df751fc0ad4aa6408
nsa25638 <nsa25638@naver.com>
2018-04-27 18:39:34 +0900

    zzzzzzzzzzzzzzzzzzzz

--- app/src/main/java/edu/android/diary/Animation.java
+++ app/src/main/java/edu/android/diary/Animation.java
@@ -1,24 +1,12 @@
 package edu.android.diary;
 
-import android.icu.text.AlphabeticIndex;
-import android.nfc.Tag;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.KeyEvent;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -26,22 +14,24 @@ import com.hitomi.cmlibrary.CircleMenu;
 import com.hitomi.cmlibrary.OnMenuSelectedListener;
 import com.hitomi.cmlibrary.OnMenuStatusChangeListener;
 
-
 import java.util.Calendar;
+import java.util.List;
 
 public class Animation extends AppCompatActivity {
     private int currentIndex;
+    private ImageView imggggg;
     private CircleMenu circleMenu;
-private static int[] IMAGE_IDS={R.drawable.moon00,
-        R.drawable.moon01, R.drawable.moon02,
-        R.drawable.moon03, R.drawable.moon04,
-        R.drawable.moon05, R.drawable.moon06, R.drawable.moon07 };
+    private static int[] IMAGE_IDS = {R.drawable.moon00,
+            R.drawable.moon01, R.drawable.moon02,
+            R.drawable.moon03, R.drawable.moon04,
+            R.drawable.moon05, R.drawable.moon06, R.drawable.moon07};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation);
 
+        imggggg =findViewById(R.id.imageView);
         java.util.Calendar cal = java.util.Calendar.getInstance();
         int year = cal.get(java.util.Calendar.YEAR);
         int month = cal.get(java.util.Calendar.MONTH) + 1;
@@ -98,21 +88,14 @@ private static int[] IMAGE_IDS={R.drawable.moon00,
 
     }
 
-    public void btnPrev(View view) {
-        if (currentIndex > 0) {
-            currentIndex--;
-            view.setBackgroundResource(IMAGE_IDS[currentIndex]);
-        } else {
-            Toast.makeText(Animation.this, "첫번째 이미지", Toast.LENGTH_SHORT).show();
-        }
-    }
+    @Override
+    protected void onStart() {
+        super.onStart();
 
-    public void btnNext(View view) {
-        if (currentIndex < IMAGE_IDS.length - 1) {
-            currentIndex++;
-            view.setBackgroundResource(IMAGE_IDS[currentIndex]);
-        } else {
-            Toast.makeText(Animation.this, "마지막 이미지", Toast.LENGTH_SHORT).show();
-        }
+        List<Diary> list = DiaryDao.getInstance().getContactList();
+        int size = list.size();
+        imggggg.setImageResource(IMAGE_IDS[size]);
+
+        Log.i(MainActivity.TAG, "list size = " + list.size());
     }
 }
