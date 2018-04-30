ba4422bab5b8808810a598a7561334700a948627
gmltbtngmltn <tndlfemd12@naver.com>
2018-04-30 16:15:14 +0900

    .

--- app/src/main/java/edu/android/diary/daySearch.java
+++ app/src/main/java/edu/android/diary/daySearch.java
@@ -1,78 +1,117 @@
 package edu.android.diary;
 
-import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.EditText;
 
 import java.util.Calendar;
 
 public class daySearch extends AppCompatActivity {
 
-    Button bfecha,bot_ki;
-    EditText efecha;
-    private int dia, mes, ano;
+    private DatePicker datePicker;
+    private Button check;
+
+    private int curYear;
+    private int curMonth;
+    private int curDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.daysearch);
 
-        bfecha = (Button)findViewById(R.id.bfeca);
-        efecha = (EditText)findViewById(R.id.efecha);
+        Calendar calendar = Calendar.getInstance();
+        curYear = calendar.get(Calendar.YEAR);
+        curMonth = calendar.get(Calendar.MONTH);
+        curDay = calendar.get(Calendar.DAY_OF_MONTH);
 
-        bfecha.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if (v == bfecha) {
-
-                    final Calendar c = Calendar.getInstance();
-                    dia = c.get(Calendar.DAY_OF_MONTH);
-                    mes = c.get(Calendar.MONTH);
-                    ano = c.get(Calendar.YEAR);
-
-                    DatePickerDialog datePickerDialog = new DatePickerDialog(daySearch.this, new DatePickerDialog.OnDateSetListener() {
-                        @Override
-                        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                            efecha.setText(year + "/" + (month + 1) + "/" + dayOfMonth);
-                        }
-
-                    }
-                            ,dia, mes, ano);
-                    datePickerDialog.show();
-
-                }//end if
-
-            }//end onClick()
+        datePicker = findViewById(R.id.datePicker);
+        datePicker.init(curYear, curMonth, curDay, new DatePicker.OnDateChangedListener() {
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                // 새로 정의한 리스너로 이벤트 전달
+                // getHour(), getMinute() 메소드는 API 23부터 지원함
+                curYear = year;
+                curMonth = monthOfYear + 1;
+                curDay = dayOfMonth;
+                //Toast.makeText(MainActivity.this, "year:" + year + "month:" + monthOfYear + "day:" + dayOfMonth, Toast.LENGTH_LONG).show();
+            }
         });
-       tranj();//기본적으로 전체기간의 일기가보여짐
+        check = findViewById(R.id.check);
+    }
 
-//        bot_ki = (Button)findViewById(R.id.btn_gi_rock_botki);
-//        bot_ki.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                tranj();//검색된 날짜의 일기가보여짐
-//            }
-//        });
+    public void next(View v) {
+        Intent intent = new Intent(daySearch.this, DirayListActivity.class);
+        intent.putExtra("SELECTED_YEAR", curYear);
+        intent.putExtra("SELECTED_MONTH", curMonth);
+        intent.putExtra("SELECTED_DAY", curDay);
 
-    }//end onCreate
-
-
-    public void tranj(){
-        FragmentManager fm=getSupportFragmentManager();
-        Fragment fragment=fm.findFragmentById(R.id.mock_rock_frag);
-        if(fragment==null){
-            FragmentTransaction transaction=fm.beginTransaction();
-            DiaryList_FragmentRecycler diraryMockRockFragment= DiaryList_FragmentRecycler.newInstance(dia, mes, ano);
-            transaction.replace(R.id.mock_rock_frag,diraryMockRockFragment);
-            transaction.commit();
-        }
+        startActivity(intent);
     }
 }
+
+
+
+//    Button bfecha,bot_ki;
+//    EditText efecha;
+//    private int dia, mes, ano;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.daysearch);
+//
+//        bfecha = (Button)findViewById(R.id.bfeca);
+//        efecha = (EditText)findViewById(R.id.efecha);
+//
+//        bfecha.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                if (v == bfecha) {
+//
+//                    final Calendar c = Calendar.getInstance();
+//                    dia = c.get(Calendar.DAY_OF_MONTH);
+//                    mes = c.get(Calendar.MONTH);
+//                    ano = c.get(Calendar.YEAR);
+//
+//                    DatePickerDialog datePickerDialog = new DatePickerDialog(daySearch.this, new DatePickerDialog.OnDateSetListener() {
+//                        @Override
+//                        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+//                            efecha.setText(year + "/" + (month + 1) + "/" + dayOfMonth);
+//                        }
+//
+//                    }
+//                            ,dia, mes, ano);
+//                    datePickerDialog.show();
+//
+//                }//end if
+//
+//            }//end onClick()
+//        });
+//       tranj();//기본적으로 전체기간의 일기가보여짐
+//
+////        bot_ki = (Button)findViewById(R.id.btn_gi_rock_botki);
+////        bot_ki.setOnClickListener(new View.OnClickListener() {
+////            @Override
+////            public void onClick(View v) {
+////                tranj();//검색된 날짜의 일기가보여짐
+////            }
+////        });
+//
+//    }//end onCreate
+//
+//
+//    public void tranj(){
+//        FragmentManager fm=getSupportFragmentManager();
+//        Fragment fragment=fm.findFragmentById(R.id.mock_rock_frag);
+//        if(fragment==null){
+//            FragmentTransaction transaction=fm.beginTransaction();
+//            DiaryList_FragmentRecycler diraryMockRockFragment= DiaryList_FragmentRecycler.newInstance(dia, mes, ano);
+//            transaction.replace(R.id.mock_rock_frag,diraryMockRockFragment);
+//            transaction.commit();
+//        }
+//    }
+
