e97447ef6e58a4e77247d8fb7fae4041acd6c67c
HolyRgingPower <soulmate106@naver.com>
2018-05-04 12:03:39 +0900

    11111111111111111111

--- app/src/main/java/edu/android/diary/DiaryList_FragmentViewpager.java
+++ app/src/main/java/edu/android/diary/DiaryList_FragmentViewpager.java
@@ -181,7 +181,6 @@ public class DiaryList_FragmentViewpager extends Fragment {
                                             }else if(comm==1){
                                                 DiaryDao.getInstance().deleteDiary(dataset,position);
                                                 dataset = DiaryDao.getInstance().getContactList(year,month,day);
-                                                
                                             }
                                             Toast.makeText(getContext(), "삭제됨", Toast.LENGTH_SHORT).show();
 
